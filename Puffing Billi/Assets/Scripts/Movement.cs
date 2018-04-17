84190ab009d31f596d89b10130c929c2aca17072
Mark Phillips <markelvisphillips@gmail.com>
2018-04-17 16:05:45 +1000

    no message

---
+++ Puffing Billi/Assets/Scripts/Movement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Vector3 targetPosition;
+    Rigidbody rb;
+    Vector3 newDir;
+    [SerializeField] float speed;
+    [SerializeField] float minRotationSpeed;
+    [SerializeField] float maxRotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetTargetPosition();
+        }
+
+        if (targetPosition != Vector3.zero)
+        {
+            RotateToTarget();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 playerPos = new Vector2(transform.position.x, transform.position.z);
+        Vector2 targetPos = new Vector2(targetPosition.x, targetPosition.z);
+
+        if (targetPosition != Vector3.zero && Vector2.Distance(playerPos, targetPos) >= 0.2f)
+        {
+            rb.MovePosition(transform.position + transform.forward * (speed * Time.deltaTime));
+        }
+
+    }
+    void SetTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000, 1 << LayerMask.NameToLayer("Raycast")))
+        {
+            targetPosition = hit.point;
+        }
+    }
+    void RotateToTarget()
+    {
+        Vector3 dir = targetPosition - transform.position;
+        float rotationSpeed = 0f;
+
+        if (transform.forward.normalized == dir.normalized)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, targetPosition) <= 2.0f)
+        {
+            rotationSpeed = maxRotationSpeed;
+        }
+        if(Vector3.Distance(transform.position, targetPosition) >= 2.0f)
+        {
+            rotationSpeed = minRotationSpeed;
+        }
+
+         dir.y = 0; // keep the direction strictly horizontal
+         Quaternion rot = Quaternion.LookRotation(dir);
+
+
+
+         newDir = Vector3.RotateTowards(transform.forward, dir, rotationSpeed * Time.deltaTime, 0.0f);
+
+         // Move our position a step closer to the target.
+         transform.rotation = Quaternion.LookRotation(newDir);
+
+         // slerp to the desired rotation over time
+         // transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1.0f * Time.deltaTime);
+
+         Debug.Log(rotationSpeed);
+       
+    }
+
+
+
+}
---
+++ Puffing Billi/Assets/Scripts/Spawner.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    Vector3 position, maxSpawnPos, minSpawnPos;
+    GameObject[] enemyPrefabs;
+    [SerializeField]
+    List<Transform> spawnPositions;
+
+    [SerializeField]
+    private float spawnDelay;
+
+    [SerializeField]
+    private int spawnAmount;
+
+    private int currentPool;
+
+    [SerializeField] private Pool[] wavesTypes = new Pool[4];
+    float posBufferHMin;
+    float posBufferHMax;
+
+    // Use this for initialization
+    void Start()
+    {
+        //int spawned = 0;
+        //spawnPositions = new List<Vector3>();
+        //maxSpawnPos = Camera.main.ViewportToWorldPoint(new Vector3(1.0f, 1.0f, 78.0f)); ;
+        //minSpawnPos = Camera.main.ViewportToWorldPoint(new Vector3(0.0f, 0.0f, 78.0f)); ;
+
+
+        //while (spawned < numToSpawn)
+        //{
+
+        //    //Left of Screen, right of screen //ScreenHeight min, max
+        //    //position = Camera.main.ViewportToWorldPoint(new Vector3(Mathf.RoundToInt(Random.Range(0.0f, 1.0f)), Random.Range(0.0f, 1.0f), 78.0f));
+        //    int enemy = GetRandomEnemy();
+        //    position = AvailablePosition(enemy);
+        //    Instantiate(enemyPrefabs[enemy], position, Quaternion.identity);
+        //    spawned++;
+        //}
+        currentPool = 0;
+        enemyPrefabs = wavesTypes[currentPool].enemyTypes;
+        InvokeRepeating("SpawnFish", spawnDelay, 1);
+    }
+
+
+    private void SpawnFish()
+    {
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            Vector3 newPos = spawnPositions[Random.Range(0, spawnPositions.Count)].position;
+            Transform fish = Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Length)], newPos, Quaternion.identity).transform;
+
+            // they're on the left
+            if (newPos.x < 0.0f)
+            {
+               
+            }
+            else
+            {
+                fish.Rotate(Vector3.up, 180);
+            }
+        }
+    }
+
+    public void PlayerGrew()
+    {
+        currentPool++;
+        enemyPrefabs = wavesTypes[currentPool].enemyTypes;
+    }
+
+    void Update()
+    {
+        Debug.Log(Screen.currentResolution.height);
+    }
+
+    //Vector3 AvailablePosition(int a_enemy)
+    //{
+
+    //    ////spawnPositions.Add(newPos);
+    //    ////float size = 0;
+
+    //    ////switch (a_enemy)
+    //    ////{
+    //    ////    case 0:
+    //    ////        size = 0.05f;
+    //    ////        break;
+    //    ////    case 1:
+    //    ////        size = 0.10f;
+    //    ////        break;
+    //    ////    case 2:
+    //    ////        size = 0.15f;
+    //    ////        break;
+    //    ////    case 3:
+    //    ////        size = 0.20f;
+    //    ////        break;
+    //    ////    case 4:
+    //    ////        size = 0.25f;
+    //    ////        break;
+    //    //// }
+
+    //    ////for (int i = 0; i < enemyPrefabs.Count; i++)
+    //    ////{ 
+    //    ////    if (newPos.y + size != spawnPositions[i].y && newPos.y - size != spawnPositions[i].y)
+    //    ////    {
+    //    ////        spawnPositions.Add(newPos);
+    //    ////        return newPos;
+    //    ////    }
+    //    ////    else
+    //    ////    {
+    //    ////        position = Camera.main.ViewportToWorldPoint(new Vector3(Mathf.RoundToInt(Random.Range(0.0f, 1.0f)), Random.Range(0.0f, 1.0f), 78.0f));
+    //    ////    }
+    //    ////}
+
+    //    //return newPos;
+    //}
+}
+
+
+
+[System.Serializable]
+struct Pool
+{
+    public string name;
+    public GameObject[] enemyTypes;
+}
