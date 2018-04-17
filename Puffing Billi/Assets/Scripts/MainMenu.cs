00fb8a8859ab9097a58777689d729d8ad6057d5b
AIE\Max Turner <max.leslie.turner@gmail.com>
2018-04-17 10:11:51 +1000

    Main menu stuff

---
+++ Puffing Billi/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public SceneFader sceneFader;
+
+	public string levelToLoad = "Main";
+	
+	// Update is called once per frame
+	public void Play () {
+		sceneFader.FadeTo (levelToLoad);
+	}
+}
